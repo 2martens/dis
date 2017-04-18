7656a6796257e4223d429c62b35d585a8748c058
Jim Martens <github@2martens.de>
2017-04-18 19:38:47 +0200

    Improved checkPassword method
    Signed-off-by: Jim Martens <github@2martens.de>

--- 02/src/de/dis2017/Main.java
+++ 02/src/de/dis2017/Main.java
@@ -3,7 +3,6 @@ package de.dis2017;
 import de.dis2017.data.EstateAgent;
 import de.dis2017.data.db.ORM;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -57,9 +56,9 @@ public class Main {
      */
 	private static boolean checkPassword() {
 	    System.out.println("Please insert the sudo password. You are entering dangerous territory.");
-        char[] passwordInput = System.console().readPassword();
-        char[] sudoPassword = "ea-sudo".toCharArray();
-        return Arrays.equals(sudoPassword, passwordInput);
+        String passwordInput = FormUtil.readPassword();
+        String sudoPassword = "ea-sudo";
+        return sudoPassword.equals(passwordInput);
 	}
 	
 	/**
