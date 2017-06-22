5d599e83348b5e25699bdda33ea66d16b82b583d
Jim Martens <github@2martens.de>
2017-06-22 15:29:34 +0200

    Added Shop datastructure
    Signed-off-by: Jim Martens <github@2martens.de>

---
+++ 07/src/de.dis2017/data/Shop.java
@@ -0,0 +1,31 @@
+package de.dis2017.data;
+
+public class Shop {
+    private int _shopID;
+    private int _cityID;
+    private String _name;
+    
+    public int get_shopID() {
+        return _shopID;
+    }
+    
+    public void set_shopID(int shopID) {
+        _shopID = shopID;
+    }
+    
+    public int get_cityID() {
+        return _cityID;
+    }
+    
+    public void set_cityID(int cityID) {
+        _cityID = cityID;
+    }
+    
+    public String get_name() {
+        return _name;
+    }
+    
+    public void set_name(String name) {
+        _name = name;
+    }
+}
