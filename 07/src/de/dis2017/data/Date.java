adf79f425e78b636ab81ecd6aca9ae460347a1c8
Jim Martens <github@2martens.de>
2017-06-26 18:05:45 +0200

    Added Date class
    Signed-off-by: Jim Martens <github@2martens.de>

---
+++ 07/src/de/dis2017/data/Date.java
@@ -0,0 +1,49 @@
+package de.dis2017.data;
+
+public class Date {
+    private int _dateID;
+    private int _day;
+    private int _month;
+    private int _quarter;
+    private int _year;
+    
+    public int get_dateID() {
+        return _dateID;
+    }
+    
+    public void set_dateID(int _dateID) {
+        this._dateID = _dateID;
+    }
+    
+    public int get_day() {
+        return _day;
+    }
+    
+    public void set_day(int _day) {
+        this._day = _day;
+    }
+    
+    public int get_month() {
+        return _month;
+    }
+    
+    public void set_month(int _month) {
+        this._month = _month;
+    }
+    
+    public int get_quarter() {
+        return _quarter;
+    }
+    
+    public void set_quarter(int _quarter) {
+        this._quarter = _quarter;
+    }
+    
+    public int get_year() {
+        return _year;
+    }
+    
+    public void set_year(int _year) {
+        this._year = _year;
+    }
+}
