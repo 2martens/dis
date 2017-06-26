a3d3c42bf3180cced04a57b3ea6037c614454137
Jim Martens <github@2martens.de>
2017-06-26 18:03:39 +0200

    Modified Shop to work with transformation
    Signed-off-by: Jim Martens <github@2martens.de>

--- 07/src/de/dis2017/data/Shop.java
+++ 07/src/de/dis2017/data/Shop.java
@@ -2,8 +2,10 @@ package de.dis2017.data;
 
 public class Shop {
     private int _shopID;
-    private int _cityID;
     private String _name;
+    private String _city;
+    private String _region;
+    private String _country;
     
     public int get_shopID() {
         return _shopID;
@@ -13,19 +15,35 @@ public class Shop {
         _shopID = shopID;
     }
     
-    public int get_cityID() {
-        return _cityID;
-    }
-    
-    public void set_cityID(int cityID) {
-        _cityID = cityID;
-    }
-    
     public String get_name() {
         return _name;
     }
     
     public void set_name(String name) {
         _name = name;
+    }
+    
+    public String get_city() {
+        return _city;
+    }
+    
+    public void set_city(String _city) {
+        this._city = _city;
+    }
+    
+    public String get_region() {
+        return _region;
+    }
+    
+    public void set_region(String _region) {
+        this._region = _region;
+    }
+    
+    public String get_country() {
+        return _country;
+    }
+    
+    public void set_country(String _country) {
+        this._country = _country;
     }
 }
